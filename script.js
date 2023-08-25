04117567732957c95cc41c24bf62417f48c1571c
Felipe Martinez <90002761+felimart2003@users.noreply.github.com>
2023-08-25 17:40:11 -0400

    Added Projects section

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+$('html, body').animate({
+    scrollTop: $("#about").offset().top
+}, 100000);
