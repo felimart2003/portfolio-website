603a6c8e0751b4de202d239d562dad4baa248e58
Felipe Martinez <90002761+felimart2003@users.noreply.github.com>
2023-08-29 16:06:44 -0400

    back to top btn

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-$('html, body').animate({
-    scrollTop: $("#about").offset().top
-}, 100000);
+// $('html, body').animate({
+//     scrollTop: $("#about").offset().top
+// }, 100000);
+
+
+const showOnPx = 150;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+window.addEventListener('scroll', scrollFunction)
+
+function scrollFunction() {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+backToTopButton.addEventListener("click", goToTop);
